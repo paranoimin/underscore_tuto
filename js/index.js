0e21d6b11a7110d28abf741d914aeb1383f890c6
Jamin <paranoimin@gmail.com>
2017-06-30 16:18:45 +0900

    second day commit

--- js/index.js
+++ js/index.js
@@ -24,11 +24,11 @@
     var mapArea = $("#mapArea");
     var mapInner = "";
     var num = [1, 2, 3];
-
     var numMulti = _.map(num, function( value, index, items ) {
         items[index] = items[index] *2;
         return value * 3;
     });
+
     mapInner = "넘버즈 : " + num + " 멀티 : " + numMulti;
     mapArea.html(mapInner);
     //-----------------------------------------------------------------------------
@@ -41,7 +41,6 @@
     var reduceRightArea = $("#reduceRightArea");
     var reduceRightInner = "";
     var numbers = [1, 2, 3];
-
     var sumReduce = _.reduce( numbers, function( total, item, index, numbers ) {
         console.log("reduce = total : " + total + " item : " + item + " index : " + index + " numbers : " + numbers);
         return total + item;
@@ -50,6 +49,7 @@
         console.log("reduce = total : " + total + " item : " + item + " index : " + index + " numbers : " + numbers);
         return total + item;
     });
+
     reduceInner = sumReduce;
     reduceArea.html("reducd : " + reduceInner);
     reduceRightInner = sumReduceRight;
@@ -61,7 +61,57 @@
     //-----------------------------------------------------------------------------
     var findArea = $("#findArea");
     var findAreaInner = "";
-    var filterArea = $("filterArea");
-    var filterArea = "";
-    
+    var filterArea = $("#filterArea");
+    var filterAreaInner = "";
+    var filterNums = [1, 2, 3, 4, 5, 6];
+    var find = _.find(filterNums, function( num ) {
+        return num % 3 == 0;
+    });
+    var filter = _.filter(filterNums, function( num ) {
+        return num % 3 == 0;
+    });
+
+    findAreaInner = "find : " + find;
+    findArea.html(findAreaInner);
+
+    filterAreaInner = "filter : " + filter;
+    filterArea.html(filterAreaInner);    
+    console.log("filget : " + filter);
+    //-----------------------------------------------------------------------------
+    //
+    // _.where + _.finWhere
+    //
+    //-----------------------------------------------------------------------------
+    var whereArea = $("#whereArea");
+    var whereAreaInner = "";
+    var findWhereArea  = $("#findWhereArea");
+    var findWhereAreaInner = "";
+    var people = [
+        {name: "tom", age: 21},
+        {name: "dick", age: 21},
+        {name: "harry", age: 80}
+    ];
+    var where = _.where(people, {age: 21});
+    var findWhere = _.findWhere(people, {age: 21});
+
+    whereAreaInner = "where : " + where; //where[0].name
+    console.log(where);
+    whereArea.html(whereAreaInner);
+    findWhereAreaInner = "findWherer : " + findWhere.name;
+    console.log(findWhere.name);
+    findWhereArea.html(findWhereAreaInner);
+    //-----------------------------------------------------------------------------
+    //
+    // _.reject
+    //
+    //-----------------------------------------------------------------------------
+    var rejectArea = $("#rejectArea");
+    var rejectAreaInner = "";
+    var rejectNims = [1, 2, 3, 4, 5, 6];
+    var odd = _.reject(rejectNims, function( num ) {
+        return num % 2 == 0;
+    });
+
+    rejectAreaInner = odd;
+    rejectArea.html(rejectAreaInner);
 }());
